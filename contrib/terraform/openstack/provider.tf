364406eb653cb17d394bbd4627c4e33043830b46
Höhl, Lukas <lukas.hoehl@accso.de>
2021-11-22 15:25:02 +0100

    provider

--- contrib/terraform/openstack/provider.tf
+++ contrib/terraform/openstack/provider.tf
@@ -1,10 +1,11 @@
 provider "openstack" {
-  application_credential_id     = "ebca0e8162b641beb2636cecab4c2fc1"
+  # TODO: Set!
+  application_credential_id     = ""
   auth_url                      = "https://prod1.api.pco.get-cloud.io:5000"
   region                        = "prod1"
   domain_name                   = "pco"
-  application_credential_secret = "A7krq9Atc3sm2Po2aL9Fu3AMzdNwpF"
+  # TODO: Set!
+  application_credential_secret = ""
   use_octavia                   = true
-
 }
 
